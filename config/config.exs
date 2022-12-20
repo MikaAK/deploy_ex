30a3484734d6fd8387b4732a037250b7c1a96fbb
MikaAK <mika@kalathil.me>
2022-12-20 15:09:52 -0800

    fix: remove forced recompile fix aws config

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,8 @@
 import Config
 
 config :ex_aws,
-  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}, :instance_role],
-  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, {:awscli, "default", 30}, :instance_role]
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, {:awscli, "default", 30}]
 
 config :ex_aws, :hackney_opts,
   follow_redirect: true,
