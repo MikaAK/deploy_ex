79ac851707e11ad8ad061bf04c2245ff1caa5088
MikaAK <mika@kalathil.me>
2022-12-20 22:46:53 -0800

    fix: aws config fix to make it work by default better

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,8 @@
 import Config
 
 config :ex_aws,
-  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}],
-  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, {:awscli, "default", 30}]
+  access_key_id: [System.get_env("AWS_ACCESS_KEY_ID"), {:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}],
+  secret_access_key: [System.get_env("AWS_SECRET_ACCESS_KEY"), {:system, "AWS_SECRET_ACCESS_KEY"}, {:awscli, "default", 30}]
 
 config :ex_aws, :hackney_opts,
   follow_redirect: true,
